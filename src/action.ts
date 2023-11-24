77dc6bc229d69eef0fe4b5a827fed5bf15fd7ba5
Muhammet Emin Akalan <05akalan57@gmail.com>
2023-11-24 22:13:27 +0300

    feat(database): connect and save data to Postgres using Prisma ORM

--- src/action.ts
+++ src/action.ts
@@ -4,28 +4,38 @@ import { Resend } from 'resend'
 import * as z from 'zod'
 
 import FormSchema from '@/lib/form-schema'
+import prisma from '@/lib/prisma'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendMail(data: z.infer<typeof FormSchema>) {
-  try {
+  const existingForm = await prisma.form.findFirst({
+    where: {
+      OR: [{ email: data.email }]
+    }
+  })
+
+  if (existingForm) {
+    return false
+  } else {
+    await prisma.form.create({ data })
     await resend.emails.send({
       from: 'MTO <no-reply@mto.akalan.dev>',
       to: [data.email],
       subject: 'Başvurunuz Alındı',
       html: `
-      Selamünaleyküm ${data.name} ${data.surname} 👋️, <br/> <br/>
+        Selamünaleyküm ${data.name} ${data.surname} 👋️, <br/> <br/>
 
-      MTO'ya şimdiden hoş geldin! 🤗️ Bu e-postayı alabiliyorsan, her şey yolunda demektir. <br/> <br/>
+        MTO'ya şimdiden hoş geldin! 🤗️ Bu e-postayı alabiliyorsan, her şey yolunda demektir. <br/> <br/>
 
-      Gelişmelerden seni en kısa sürede haberdar edeceğiz. <br/> <br/>
+        Gelişmelerden seni en kısa sürede haberdar edeceğiz. <br/> <br/>
 
-      Selametle <br/> <br/>
+        Selametle <br/> <br/>
 
-      <pre>${JSON.stringify(data, null, 4)}</pre>
-      `
+        <pre>${JSON.stringify(data, null, 4)}</pre>
+        `
     })
-  } catch (error) {
-    console.error(error)
+
+    return true
   }
 }
